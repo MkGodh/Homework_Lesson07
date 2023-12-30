4ee92241e855aa33180364b5fe779627f8e9730d
MkGodh <fenryrvla@yandex.ru>
2023-12-30 13:14:17 +0300

    Рекурсия ДЗ

---
+++ Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать
+//  на вход число и возвращать сумму его цифр. 
+//  Указание Использовать рекурсию.
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumOfDigits(int num)
+{
+    if(num < 10)
+        return num;
+
+    int sum = (num % 10) + sumOfDigits(num / 10);
+    
+    return sum;
+}
+
+Console.WriteLine(sumOfDigits(number));
---
+++ Task_03/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        PrintReverse(array, array.Length - 1);
+    }
+
+    static void PrintReverse(int[] array, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+
+        Console.WriteLine(array[index]);
+        PrintReverse(array, index - 1);
+    }
+} 
